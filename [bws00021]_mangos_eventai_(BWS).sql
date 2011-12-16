976e831e65b1d3dd60fd6ec71bf87923a89998ab
PSZ <zavatski@gmail.com>
2011-12-16 16:38:30 +0200

    [bws21] QUEST A Dire Situation (10506) - Bloodmaul Dire Wolf give quest credit on Rina\'s Diminution Powder spell hit.

--- [bws00021]_mangos_eventai_(BWS).sql
+++ [bws00021]_mangos_eventai_(BWS).sql
@@ -166,3 +166,8 @@ INSERT INTO creature_ai_scripts (id, creature_id, event_type, event_inverse_phas
 (1998950, 19989, 6, 0, 100, 0, 0, 0, 0, 0, 11, 37473, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Grishna Harbinger - Cast Detect Whispers on Death'),
 (1999050, 19990, 6, 0, 100, 0, 0, 0, 0, 0, 11, 37473, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Grishna Scorncrow - Cast Detect Whispers on Death'),
 (2032950, 20329, 6, 0, 100, 0, 0, 0, 0, 0, 11, 37473, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Grishna Matriarch - Cast Detect Whispers on Death');
+
+-- QUEST A Dire Situation (10506) - Bloodmaul Dire Wolf give quest credit on Rina\'s Diminution Powder spell hit
+DELETE FROM creature_ai_scripts WHERE id IN (2005850);
+INSERT INTO creature_ai_scripts (id, creature_id, event_type, event_inverse_phase_mask, event_chance, event_flags, event_param1, event_param2, event_param3, event_param4, action1_type, action1_param1, action1_param2, action1_param3, action2_type, action2_param1, action2_param2, action2_param3, action3_type, action3_param1, action3_param2, action3_param3, COMMENT) VALUES
+(2005850, 20058, 8, 0, 100, 1, 36310, -1, 60000, 60000, 33, 21176, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Bloodmaul Dire Wolf - give quest A Dire Situation (10506) credit on Rina\'s Diminution Powder spell hit');
